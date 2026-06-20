d50d020117714ac94e5bf3aad0cef0709554c9ea
koushaljhacs <koushaljha.cs@gmail.com>
2026-06-20 20:32:25 +0530

    setup: add vite react frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main>
+      <h1>Welcome to Koshiv Roadlines</h1>
+      <p>This is the React + TypeScript Vite frontend.</p>
+    </main>
+  );
+}
+
+export default App;
